0c158979495f4cc2cdb5ed4912afe996e266298d
shantanuk7 <shantanukulkarni365@gmail.com>
2026-01-23 15:24:32 +0530

    feat: Add displayBooks method

--- Library.java
+++ Library.java
@@ -7,4 +7,15 @@ public class Library {
     public void addBook(String title, int copies) {
         books.add(new Book(title, copies));
     }
+
+    public void displayBooks() {
+        if (books.isEmpty()) {
+            System.out.println("No books in the library.");
+            return;
+        }
+
+        for (Book book : books) {
+            System.out.println(book.displayInfo());
+        }
+    }
 }
