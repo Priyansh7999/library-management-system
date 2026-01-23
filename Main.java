898a77ad2d37c59ed9c8598016e7080927468737
Rakshit Saxena <rakshitsaxena96@gmail.com>
2026-01-23 15:36:06 +0530

    feat: display library menu

--- Main.java
+++ Main.java
@@ -1,5 +1,28 @@
+import java.util.Scanner;
+
 public class Main {
+
     public static void main(String[] args) {
-        System.out.print("Welcome to Library Management System");
+
+        Library library = new Library();
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            System.out.println("\nLibrary Management System");
+            System.out.println("1. list books");
+            System.out.println("2. add book");
+            System.out.println("3. to borrow book");
+            System.out.println("4. to return book");
+            System.out.println("5. exit");
+            System.out.print("Enter your choice(in numbers from 1 to 5): ");
+
+            int choice = scanner.nextInt();
+            scanner.nextLine();
+            return;
+
+
+            
+            }
+        }
     }
-}
+
