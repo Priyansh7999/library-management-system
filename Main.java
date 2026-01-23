7796755be6eb1a1fdaec4f082d8728607cd377da
Rakshit Saxena <rakshitsaxena96@gmail.com>
2026-01-23 15:48:20 +0530

    feat: Add Borrow and return call

--- Main.java
+++ Main.java
@@ -37,9 +37,54 @@ public class Main {
                     library.addBook(title, copies);
                     System.out.println("Book added successfully.");
                     break;
+                 case 3: // to borrow book
+                    System.out.print("Enter student ID: ");
+                    String studentId = scanner.nextLine();
+
+                    System.out.print("Enter student name: ");
+                    String studentName = scanner.nextLine();
+
+                    Student student = new Student(studentId, studentName);
+
+                    System.out.print("Enter book title: ");
+                    String borrowTitle = scanner.nextLine();
+
+                    if (library.borrowBook(borrowTitle, student)) {
+                        System.out.println("Book borrowed successfully.");
+                    } else {
+                        System.out.println("Book not available or already borrowed.");
+                    }
+                    break;
+
+                case 4: // to return book
+                    System.out.print("Enter student ID: ");
+                    String returnId = scanner.nextLine();
+
+                    System.out.print("Enter student name: ");
+                    String returnName = scanner.nextLine();
+
+                    Student returnStudent = new Student(returnId, returnName);
+
+                    System.out.print("Enter book title: ");
+                    String returnTitle = scanner.nextLine();
+
+                    if (library.returnBook(returnTitle, returnStudent)) {
+                        System.out.println("Book returned successfully.");
+                    } else {
+                        System.out.println("Invalid return request.");
+                    }
+                    break;
+
+                case 5: 
+                    System.out.println("Exiting system. Goodbye!");
+                    scanner.close();
+                    return;
+
+                default:
+                    System.out.println("Invalid choice. Try again.");
 
             }
-            return;
+            
         }
     }
 }
