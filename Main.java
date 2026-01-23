818f07e70b4be1a7a7c330621e5e850b05888ca0
Rakshit Saxena <rakshitsaxena96@gmail.com>
2026-01-23 15:43:38 +0530

    feat: add display feature call

--- Main.java
+++ Main.java
@@ -9,20 +9,24 @@ public class Main {
 
         while (true) {
             System.out.println("\nLibrary Management System");
+
             System.out.println("1. list books");
             System.out.println("2. add book");
             System.out.println("3. to borrow book");
+
             System.out.println("4. to return book");
             System.out.println("5. exit");
+            
             System.out.print("Enter your choice(in numbers from 1 to 5): ");
 
             int choice = scanner.nextInt();
             scanner.nextLine();
-            return;
-
-
-            
+            switch (choice) {
+                case 1: // to display books
+                    library.displayBooks();
+                    break;
             }
+            return;
         }
     }
-
+}
