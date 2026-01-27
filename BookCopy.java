b82c811ea188562bc1442e897e84c868169103a0
shantanuk7 <shantanukulkarni365@gmail.com>
2026-01-27 12:28:37 +0530

    feat: Add BookCopy entity

---
+++ BookCopy.java
@@ -0,0 +1,12 @@
+public class BookCopy {
+    private String bookCopyId;
+
+    public BookCopy(String id) {
+        this.bookCopyId = id;
+    }
+
+    public String getBookId(){
+        return this.bookCopyId;
+    }
+
+}
