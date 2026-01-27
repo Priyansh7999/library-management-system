ce62d800e5cf1d691c8722f16e2b11cf8c9f5788
Priyansh Saxena <priyanshsaxena7999@gmail.com>
2026-01-27 13:44:46 +0530

    feat: add mobile number to Member

--- Member.java
+++ Member.java
@@ -2,11 +2,13 @@ public abstract class Member {
     private final String id;
     private String name;
     private String type;
+    private String mobileNumber;
 
-    public Member(String id, String name, String type) {
+    public Member(String id, String name, String type, String mobileNumber) {
         this.id = id;
         this.name = name;
         this.type = type;
+        this.mobileNumber = mobileNumber;
     }
 
     public String getId() { 
@@ -15,8 +17,11 @@ public abstract class Member {
     public String getName() { 
         return name; 
     }
+    public String getMobileNumber() { 
+        return mobileNumber; 
+    }
 
     public String memberDetails() {
-        return "Member ID: " + id + ", Name: " + name + ", Type: " + type;
+        return "Member ID: " + id + ", Name: " + name + ", Type: " + type + ", Mobile: " + mobileNumber;
     }
 }
