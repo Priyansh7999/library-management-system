4e9b41901a0e52e569ec967bcbe491c5bcee83c4
shantanuk7 <shantanukulkarni365@gmail.com>
2026-01-27 14:14:08 +0530

    feat: Add basic validation logic while adding new book

--- Library.java
+++ Library.java
@@ -7,11 +7,19 @@ public class Library {
     public void addBook(String title) {
         // Add refined addBook logic
 
-        // validation - does book with same name already exist
+        // validation - does book with same name already exist]
+        if (hasBookWithTitle(title)) {
+            System.out.println("The book with this title already exists");
+            return;
+        }
 
         // generate a simple key value pair such as {"Book Title": ["Id 1", "Id 2"]}
 
         // Add the book map to the hashmap of books.
+    }
+
+    public boolean hasBookWithTitle(String title) {
+        return books.containsKey(title);
     }
 
     public void displayBooks() {
