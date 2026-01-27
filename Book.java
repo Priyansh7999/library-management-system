52da21f541f136f5345bc808244bb268e4b35469
shantanuk7 <shantanukulkarni365@gmail.com>
2026-01-27 13:43:13 +0530

    fix: remove book availability count from Book entity

--- Book.java
+++ Book.java
@@ -1,13 +1,9 @@
 public class Book {
     private String bookId;
     private String title;
-    private int totalCopies;
-    private int availableCopies;
 
     public Book(String title, int copies) {
         this.title = title;
-        this.totalCopies = copies;
-        this.availableCopies = copies;
     }
 
     public String getBookId() {
@@ -16,30 +12,5 @@ public class Book {
 
     public String getTitle() {
         return title;
-    }
-
-    public int getAvailableCopies() {
-        return availableCopies;
-    }
-
-    public void borrowCopy() {
-        if (availableCopies > 0) {
-            availableCopies--;
-        }
-    }
-
-    public void returnCopy() {
-        if (availableCopies < totalCopies) {
-            availableCopies++;
-        }
-    }
-
-    public boolean isAvailable() {
-        return availableCopies > 0;
-    }
-
-    // to display particular book info
-    public String displayInfo() {
-        return title + " | Available: " + availableCopies + "/" + totalCopies;
     }
 }
