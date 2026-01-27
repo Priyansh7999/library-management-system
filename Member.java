e8b1aea3e9dea57009687d6a198f65f59c736301
Priyansh Saxena <priyanshsaxena7999@gmail.com>
2026-01-27 12:12:26 +0530

    feat: Add member class with name and member id

--- Member.java
+++ Member.java
@@ -1,16 +1,23 @@
-public class Member {
-    protected String memberId;  //<-- Unique identifier for the member (any string)
-    protected String name;      //<-- Names of the member (full name)
+public abstract class Member {
+    private final String memberId;
+    private String name;
 
-    public Member(String memberId, String name) { //<-- Constructor
+    public Member(String memberId, String name) {
         this.memberId = memberId;
         this.name = name;
-    }  
-    //getter for attributes
-    public String getMemberId() {
-        return memberId;
-    }   
-    public String getName() {
-        return name;
+    }
+
+    public String getMemberId() { 
+        return memberId; 
+    }
+    public String getName() { 
+        return name; 
+    }
+    public void setName(String name) { 
+        this.name = name; 
+    }
+
+    public String memberDetails() {
+        return "Member ID: " + memberId + ", Name: " + name;
     }
 }
