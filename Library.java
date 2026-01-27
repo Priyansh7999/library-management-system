135953d1cb2d70d3397b39d36b1df628159f1064
shantanuk7 <shantanukulkarni365@gmail.com>
2026-01-27 14:51:19 +0530

    feat: Add method to generate book copy Ids

--- Library.java
+++ Library.java
@@ -2,9 +2,9 @@ import java.util.*;
 
 public class Library {
 
-    private Map<String, List<String>> books = new HashMap<>();
+    private Map<String, List<String>> availableBooksByTitle = new HashMap<>();
     
-    public void addBook(String title) {
+    public void addBook(String id, String title, String availableCopies) {
         // Add refined addBook logic
 
         // validation - does book with same name already exist]
@@ -13,19 +13,35 @@ public class Library {
             return;
         }
 
+        // Create new book object
+        Book book = new Book(id, title);
+
         // generate a simple key value pair such as {"Book Title": ["Id 1", "Id 2"]}
+        generateBookCopyIds(title, availableCopies);
 
         // Add the book map to the hashmap of books.
     }
 
     public boolean hasBookWithTitle(String title) {
-        return books.containsKey(title);
+        return availableBooksByTitle.containsKey(title);
     }
 
     public void displayBooks() {
-        if (books.isEmpty()) {
+        if (availableBooksByTitle.isEmpty()) {
             System.out.println("No books in the library.");
             return;
         }
+    }
+
+    public void generateBookCopyIds(String title, String availableCopies) {
+        int copies = Integer.parseInt(availableCopies);
+
+        List<String> copyId = new ArrayList<>();
+        
+        for (int i=1; i<=copies; i++){
+            copyId.add(title + String.valueOf(i));
+        }
+
+        availableBooksByTitle.put(title, copyId);
     }
 }
