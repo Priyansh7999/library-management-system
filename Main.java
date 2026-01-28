a2c1cdd8f2f9829a0b61479a845567924ab2ff93
Rakshit Saxena <rakshitsaxena96@gmail.com>
2026-01-28 10:21:22 +0530

    fix:comment return book method in main

--- Main.java
+++ Main.java
@@ -14,10 +14,10 @@ public class Main {
             System.out.println("2. add book");
             System.out.println("3. to borrow book");
 
-            System.out.println("4. to return book");
-            System.out.println("5. exit");
+           // System.out.println("4. to return book");
+           System.out.println("4. exit");
 
-            System.out.print("Enter your choice(in numbers from 1 to 5): ");
+            System.out.print("Enter your choice(in numbers from 1 to 4): ");
 
             int choice = scanner.nextInt();
             scanner.nextLine();
@@ -56,26 +56,26 @@ public class Main {
                     }
                     break;
 
-                case 4: // to return book
-                    System.out.print("Enter student ID: ");
-                    String returnId = scanner.nextLine();
+                // case 4: // to return book
+                //     System.out.print("Enter student ID: ");
+                //     String returnId = scanner.nextLine();
 
-                    System.out.print("Enter student name: ");
-                    String returnName = scanner.nextLine();
+                //     System.out.print("Enter student name: ");
+                //     String returnName = scanner.nextLine();
 
-                    Student returnStudent = new Student(returnId, returnName);
+                //     Student returnStudent = new Student(returnId, returnName);
 
-                    System.out.print("Enter book title: ");
-                    String returnTitle = scanner.nextLine();
+                //     System.out.print("Enter book title: ");
+                //     String returnTitle = scanner.nextLine();
 
-                    if (library.returnBook(returnTitle, returnStudent)) {
-                        System.out.println("Book returned successfully.");
-                    } else {
-                        System.out.println("Invalid return request.");
-                    }
-                    break;
+                //     if (library.returnBook(returnTitle, returnStudent)) {
+                //         System.out.println("Book returned successfully.");
+                //     } else {
+                //         System.out.println("Invalid return request.");
+                //     }
+                //     break;
 
-                case 5: 
+                case 4: 
                     System.out.println("Exiting system. Goodbye!");
                     scanner.close();
                     return;
